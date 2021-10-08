764013c9f2e28499a0b284cf6a8698f8a9c0167a
MaqsudMallick <88634721+MaqsudMallick@users.noreply.github.com>
2021-10-08 10:54:53 +0530

    Responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,11 +5,20 @@ import { Button } from '@mui/material';
 import { ButtonGroup } from '@mui/material';
 import  Link from 'next/link';
 import Image from 'next/image';
+import { IconButton } from '@mui/material';
+
+
+
+
+import { CssBaseline } from '@mui/material';
+
 const Navbar = () => {
   return (
     <AppBar position="static" color= "success">
+      <CssBaseline />
+      {/*   */}
       <Toolbar>
-          <ButtonGroup>
+          {/* <ButtonGroup>
               <Link href="/"  passHref>
         <Button size= "large" variant= "contained" color= "primary">Home</Button>
         </Link>
@@ -26,6 +35,35 @@ const Navbar = () => {
         <Button size= "large" variant= "contained" color= "primary">About</Button>
         </Link>
         </ButtonGroup>
+         */}
+         
+         <div className="navlinks">
+         <div className= "nav-nav">
+         <Link href="/"  passHref>
+        Home  
+        </Link>
+        </div>
+        <div className= "nav-nav nav-1">
+        <Link href="/education" passHref>
+        Education 
+        </Link>
+        </div>
+        <div className= "nav-nav nav-2">
+        <Link href="/gallery" passHref>
+        Gallery 
+        </Link>
+        </div>
+        <div className= "nav-nav nav-3">
+        <Link href="/projects" passHref>
+        Projects 
+        </Link>
+        </div>
+        <div className= "nav-nav nav-4">
+        <Link href="/about" passHref>
+        About
+        </Link>
+        </div>
+         </div>
       </Toolbar>
     </AppBar>
   );
--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,10 @@ export default function Home() {
   return (
       <div className= "home">
         <Navbar />
-        <Image src="/image.jpg" width= "1500" height= "638" quality= "1" alt="decoration"/>
+        <Image className= "front-matter" src="/image.jpg" width= "1500" height= "638" quality= "1" alt="decoration"/>
 
 
-        <h1 style= {{color: "green", top: "160px", fontWeight: "800", fontSize: "80px"}}>Maqsud Mallick!</h1>
+        <h1 className= "title" style= {{color: "green", fontWeight: "800", fontSize: "80px"}}>Maqsud Mallick!</h1>
         <h1>Personal Site</h1>
       <Footer />
       </div>
